4b1fe17e767a58f44e3cbb2f3e80944a8a465897
LellaHemasriSai <91315524+LellaHemasriSai@users.noreply.github.com>
2022-01-24 10:28:34 +0530

    Update atm.java

--- atm.java
+++ atm.java
@@ -8,21 +8,22 @@ public class atm {
         this.users = new ArrayList<user>();
         Dispenser = dispenser;
     }
-    public boolean enter(int accountnumber, int pin, ArrayList<user> listOfcustomers){
-		int AccountNumber;
-		int AccountPin;
-		boolean v = false;
-		for(int i=0; i<listOfcustomers.size(); i++)
+    public boolean enter(int accountnumber, int pin, ArrayList<user> listOfcustomers)
+    {
+	int AccountNumber;
+	int AccountPin;
+	boolean v = false;
+	for(int i=0; i<listOfcustomers.size(); i++)
         {
-			user u = listOfcustomers.get(i);
-			accountdetails a = u.Account;
-			AccountNumber = a.getAccnumber();
-			AccountPin = a.getPin();
-			if(AccountNumber == accountnumber && AccountPin == pin)
-            {
-				v = true;
-			}
+		user u = listOfcustomers.get(i);
+		accountdetails a = u.Account;
+		AccountNumber = a.getAccnumber();
+		AccountPin = a.getPin();
+		if(AccountNumber == accountnumber && AccountPin == pin)
+             	{
+			v = true;
 		}
-		return v;
 	}
+	return v;
+   }
 }
